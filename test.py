6545a93c958aa88bdd3e83f05ed3f63ef7374ed7
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-04-10 19:36:36 -0700

    Add some 'insert' tests

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ class TestTable(object):
             ('id', int),
             ('name', str),
             ('age', int),
-            ('height', float))
+            ('height', float)
+        )
         self.tbl = Table.create(
             "test.db", "Foo", self.dtypes, primary_key='id')
 
@@ -35,3 +36,68 @@ class TestTable(object):
     def test_drop(self):
         self.tbl.drop()
         self.tbl.select()
+
+    @with_setup(setup, teardown)
+    def test_insert_null(self):
+        self.tbl.insert({})
+
+    @with_setup(setup, teardown)
+    @raises(ValueError)
+    def test_insert_string(self):
+        self.tbl.insert('Alyssa P. Hacker')
+
+    @raises(ValueError)
+    def test_insert_int(self):
+        self.tbl.insert(25)
+
+    @raises(ValueError)
+    def test_insert_float(self):
+        self.tbl.insert(66.25)
+
+    @with_setup(setup, teardown)
+    def test_insert_dict(self):
+        self.tbl.insert({
+            'name': 'Alyssa P. Hacker',
+            'age': 25,
+            'height': 66.25
+        })
+
+    @with_setup(setup, teardown)
+    def test_insert_dictlist(self):
+        self.tbl.insert([
+            {
+                'name': 'Alyssa P. Hacker',
+                'age': 25,
+                'height': 66.25
+            },
+            {
+                'name': 'Ben Bitdiddle',
+                'age': 24,
+                'height': 70.1
+            }])
+
+    @with_setup(setup, teardown)
+    @raises(ValueError)
+    def test_insert_shortlist(self):
+        self.tbl.insert(['Alyssa P. Hacker', 25])
+
+    @with_setup(setup, teardown)
+    @raises(ValueError)
+    def test_insert_shortlists(self):
+        self.tbl.insert([
+            ['Alyssa P. Hacker', 66.25],
+            ['Ben Bitdiddle', 24]
+        ])
+
+    @with_setup(setup, teardown)
+    def test_insert_list(self):
+        self.tbl.insert(['Alyssa P. Hacker', 25, 66.25])
+
+    @with_setup(setup, teardown)
+    def test_insert_lists(self):
+        self.tbl.insert([
+            ['Alyssa P. Hacker', 25, 66.25],
+            ['Ben Bitdiddle', 24, 70.1]
+        ])
+
+    
