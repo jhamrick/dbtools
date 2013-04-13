1710afaee5462212fb94815c7d3b2873d45cfa96
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-04-12 22:54:50 -0700

    Add comments/docstrings

--- table.py
+++ table.py
@@ -5,12 +5,29 @@ import re
 
 
 class Table(object):
+    """A frame-like interface to a SQLite database table.
+
+    """
 
     def __init__(self, db, name, verbose=False):
+        """Create an interface to the table `name` in the database `db`.
+
+        Parameters
+
+            db: the path to the SQLite database
+
+            name: the name of the table in the database
+
+            verbose: (default=False) print out SQL command information
+
+        """
+
+        # save the parameters
         self.db = str(db)
         self.name = str(name)
         self.verbose = bool(verbose)
 
+        # query the database for information about the table
         conn = sql.connect(self.db)
         with conn:
             cur = conn.cursor()
@@ -18,12 +35,16 @@ class Table(object):
                         "WHERE tbl_name='%s' and type='table'" % name)
             info = cur.fetchall()
 
+        # parse the response -- it will look like 'CREATE TABLE
+        # name(col1 TYPE, col2 TYPE, ...)'
         args = re.match("([^\(]*)\((.*)\)", info[0][0]).groups()[1]
-        cols = args.split(", ")
-        self.columns = tuple([x.split(" ")[0] for x in cols])
 
         # compute repr string
         self.repr = "%s(%s)" % (self.name, args)
+
+        # get the column names
+        cols = args.split(", ")
+        self.columns = tuple([x.split(" ")[0] for x in cols])
 
         # parse primary key, if any
         pk = [x.split(" ", 1)[1].find("PRIMARY KEY") > -1 for x in cols]
@@ -57,6 +78,29 @@ class Table(object):
     @classmethod
     def create(cls, db, name, dtypes, primary_key=None,
                autoincrement=False, verbose=False):
+        """Create a table called `name` in the database `db`.
+
+        Parameters
+
+            db: path to the SQLite database
+
+            name: name of the desired table
+
+            dtypes: list of 2-tuples, where each tuple corresponds a
+            desired column and has the format (column name, data type).
+
+            primary_key: (default=None) name of the primary key
+            column. If None, no primary key is set.
+
+            autoincrement: (default=False) set the primary key column to
+            automatically increment.
+
+            verbose: (default=False) print out SQL command information.
+
+        Returns: Table object
+
+        """
+
         args = []
 
         for label, dtype in dtypes:
@@ -95,10 +139,18 @@ class Table(object):
                 print cmd
             cur.execute(cmd)
 
-        tbl = cls(db, name)
+        tbl = cls(db, name, verbose=verbose)
         return tbl
 
     def drop(self):
+        """Drop the table from its database.
+
+        Note that after calling this method, this Table object will no
+        longer function properly, as it will correspond to a table that
+        does not exist.
+
+        """
+
         conn = sql.connect(self.db)
         with conn:
             cur = conn.cursor()
@@ -108,6 +160,25 @@ class Table(object):
             cur.execute(cmd)
 
     def insert(self, values=None):
+        """Insert values into the table.
+
+        The `values` parameter should be a list of non-string sequences
+        (or a single sequence, which will then be encased in a
+        list). Each sequence is handled as follows:
+
+            - If the sequence is a dictionary, then the keys should
+              correspond to column names and the values should match the
+              data types of the columns.
+
+            - If the sequence is not a dictionary, it should have length
+              equal to the number of columns and each element should be
+              a value to be inserted in the corresponding column.
+
+        If the table has an autoincrementing primary key, then this
+        value should be excluded from every sequence as it will be
+        filled in automatically.
+
+        """
 
         # argument parsing -- `values` should be a list of sequences
         if values is None:
